021f70265a4d03093f7ece24f2663ac77886b442
KimYouGyoung <tptkds12@gmail.com>
2024-02-10 18:41:13 +0900

    비디오컨트롤 구현 완료

--- script.js
+++ script.js
@@ -7,8 +7,19 @@
   const $search = get('#search');
   const $list = getAll('.contents.list figure');
   const $searchButton = get('.btn_search');
+
+  const $player = get('.view video');
+  const $btnPlay = get('.js-play');
+  const $btnReplay = get('.js-replay');
+  const $btnStop = get('.js-stop');
+  const $btnMute = get('.js-mute');
+  const $progress = get('.js-progress');
+  const $volume = get('.js-volume');
+  const $fullscreen = get('.js-fullScreen');
+  
   
   const init = () => {
+    console.log('init');
     $search.addEventListener('keyup', search);
     $searchButton.addEventListener('click', search);
     for (let index = 0; index < $list.length; index++) {
@@ -17,6 +28,7 @@
       $target.addEventListener('mouseout', onMouseOut);
     }
     for (let index = 0; index < $list.length; index++) {
+      console.log('click')
       $list[index].addEventListener('click', hashChange);
     }
 
@@ -28,6 +40,7 @@
       } else {
         getListView();
       }
+      viewPageEvent();
     })
   }
 
@@ -59,6 +72,7 @@
     e.preventDefault();
     const parentNode = e.target.closest('figure');
     const viewTitle = parentNode.querySelector('strong').textContent;
+    console.log(viewTitle);
     window.location.hash = `view${viewTitle}`;
     getViewPage();
   }
@@ -76,6 +90,109 @@
     get('.view').style.display = 'none';
     get('.list').style.display = 'flex';
   }
+
+  const buttonChange = (btn, value) => {
+    btn.innerHTML = value;
+  }
+  const viewPageEvent = () => {
+    $volume.addEventListener('change', e => {
+      $player.$volume = e.target.value;
+    });
+
+    $player.addEventListener('timeupdate', setProgress);
+    $player.addEventListener('play', buttonChange($btnPlay, 'pause'));
+    $player.addEventListener('pause', buttonChange($btnPlay, 'play'));
+    $player.addEventListener('volumeChange', () => {
+      $player.muted ? buttonChange($btnMute, 'unmute') : buttonChange($btnMute, 'mute');
+    });
+    $player.addEventListener('ended', $player.pause());
+    $progress.addEventListener('click', getCurrent);
+    $btnPlay.addEventListener('click', playVideo);
+    $btnStop.addEventListener('click', stopVideo);
+    $btnReplay.addEventListener('click', replayVideo);
+    $btnMute.addEventListener('click', mute);
+    $fullscreen.addEventListener('click', fullScreen);
+    init();
+  }
+
+  const getCurrent = (e) => {
+    let percent = e.offsetX / $progress.offsetWidth;
+    $player.currentTime = percent * $player.duration;
+    e.target.value = Math.floor(percent / 100);
+  }
+
+  const setProgress = () => {
+    let percentage = Math.floor((100 / $player.duration) * $player.currentTime);
+    $progress.value = percentage;
+  }
+
+  const playVideo = () => {
+    if ($player.paused || $player.ended) {
+      buttonChange($btnPlay, 'pause');
+      $player.play();
+    } else {
+      buttonChange($btnPlay, 'play');
+      $player.pause();
+    }
+  }
+
+  const stopVideo = () => {
+    $player.pause();
+    $player.currentTime = 0;
+    buttonChange($btnPlay, 'play');
+  }
+
+  const resetPlayer = () => {
+    $progress.value = 0;
+    $player.currentTime = 0;
+  }
+  
+  const replayVideo = () => {
+    resetPlayer();
+    $player.play();
+    buttonChange($btnPlay, 'pause');
+  }
+
+  const mute = () => {
+    if ($player.muted) {
+      buttonChange($btnMute, 'mute');
+      $player.muted = false;
+    } else {
+      buttonChange($btnMute, 'unmute');
+      $player.muted = true;
+    }
+  }
+
+  const fullScreen = () => {
+    if ($player.requestFullscreen)
+      if (document.fullScreenElement) {
+        document.cancelFullScreen()
+      } else {
+        $player.requestFullscreen()
+      }
+    else if ($player.msRequestFullscreen)
+      if (document.msFullscreenElement) {
+        document.msExitFullscreen()
+      } else {
+        $player.msRequestFullscreen()
+      }
+    else if ($player.mozRequestFullScreen)
+      if (document.mozFullScreenElement) {
+        document.mozCancelFullScreen()
+      } else {
+        $player.mozRequestFullScreen()
+      }
+    else if ($player.webkitRequestFullscreen)
+      if (document.webkitFullscreenElement) {
+        document.webkitCancelFullScreen()
+      } else {
+        $player.webkitRequestFullscreen()
+      }
+    else {
+      alert('Not Supported')
+    }
+  }
+
   init();
 })()
 

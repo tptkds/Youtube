3aae89f3c17cb8f427dbec82d479a640a48c3b53
KimYouGyoung <tptkds12@gmail.com>
2024-02-10 16:38:55 +0900

    마우스 오버 시 영상 재생

--- script.js
+++ script.js
@@ -11,6 +11,11 @@
   const init = () => {
     $search.addEventListener('keyup', search);
     $searchButton.addEventListener('click', search);
+    for (let index = 0; index < $list.length; index++) {
+      const $target = $list[index].querySelector('picture');
+      $target.addEventListener('mouseover', onMouseOver);
+      $target.addEventListener('mouseout', onMouseOut);
+    }
   }
 
   const search = () => {
@@ -25,7 +30,16 @@
         $list[index].style.display = 'none';
       }
     }
+  }
 
+  const onMouseOver = (e) => {
+    const webpPlay = e.target.parentNode.querySelector('source');
+    webpPlay.setAttribute('srcset', './assets/sample.webp');
+  }
+
+  const onMouseOut = (e) => {
+    const webpPlay = e.target.parentNode.querySelector('source');
+    webpPlay.setAttribute('srcset', './assets/sample.jpg');
   }
   init();
 })()
